45348e6602e29124ee6b50ca8b189bf6d7a8d1a0
ios19 <ios19@Thetoan.local>
2013-11-19 20:01:34 +0700

    szcxzc

---
+++ cxzc/cxzc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cxzc
+//
+//  Created by ios19 on 11/19/13.
+//  Copyright (c) 2013 ios19. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
